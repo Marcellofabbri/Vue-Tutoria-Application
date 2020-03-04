3f3c2fa4fc95e75f453397f1f0bad3d4e19cd823
Marcellofabbri <marcellogfabbri@gmail.com>
2020-03-04 21:54:21 +0000

    add links

--- public/app.js
+++ public/app.js
@@ -2,11 +2,13 @@ new Vue({
   el: "#vue-app",
   data: {
     name: 'Marcello',
-    job: 'student'
+    job: 'student',
+    website: 'www.google.com',
+    websiteTag: '<a href="www.google.com"></a>'
   },
   methods: {
     greet: function(time) {
-      return 'Good ' + time + ', '
+      return 'Good ' + time + ', ' + this.name
     }
   }
 });
